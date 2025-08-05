ad5de1175e072c1966ab299f7851c03843234fb6
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-08-04 21:40:49 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Phoenix Creek Supply</h1>
+      <p>Rugged Tools for the Modern Stoic.</p>
+    </div>
+  );
+}
